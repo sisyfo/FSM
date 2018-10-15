9b622ce04d15a835c18f00e98f88d735b90c61a3
Jose Chocron <Jose.Chocron@eu.jll.com>
2018-10-15 22:24:40 +0200

    minor translation

--- ConsoleTestApp/Program.cs
+++ ConsoleTestApp/Program.cs
@@ -7,25 +7,25 @@ namespace ConsoleTestApp
 {
     class Program
     {
-        enum States { estado1, estado2 };
+        enum States { state1, state2 };
         enum Events { ev1, ev2 };
 
         static void Main(string[] args)
         {
             var sts = State<States, Events>.
-                When(States.estado1).
+                When(States.state1).
                     Then(() => Console.WriteLine("then state 1")).
                     Before(() => Console.WriteLine("before state 1")).
                     After(() => Console.WriteLine("after state 1")).
-                    TransitionTo(States.estado2, Events.ev1).
-                When(States.estado2).
+                    TransitionTo(States.state2, Events.ev1).
+                When(States.state2).
                     Then(() => Console.WriteLine("then state 2")).
                     Before(() => Console.WriteLine("before state 2")).
                     After(() => Console.WriteLine("after state 2")).
-                    TransitionTo(States.estado1, Events.ev2).
-                    Fallback(States.estado1, 5000);
+                    TransitionTo(States.state1, Events.ev2).
+                    Fallback(States.state1, 5000);
 
-            var fsm = new FiniteStateMachine<States, Events>(sts, States.estado1);
+            var fsm = new FiniteStateMachine<States, Events>(sts, States.state1);
 
             // TriggerEvent is Thread-Safe
             new Thread(() => fsm.TriggerEvent(Events.ev1)).Start();
